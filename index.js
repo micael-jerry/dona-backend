37505d83be2581ae7eada27e9b492b611950f799
Micael Jerry <hei.jerry.2@gmail.com>
2023-11-26 19:13:49 +0300

    fix: connect to database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { connectDB } = require('./src/config/db');
+
 const app = express();
-
 require('dotenv').config()
+connectDB();
 
-app.listen(process.env.PORT || 8080, (req, res) => {
+app.listen(process.env.PORT || 8080, () => {
     console.log(`Server started on port ${process.env.PORT || 8080}`);
 });
---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    await mongoose.connect(process.env.DATABASE_URL, {
+        bufferCommands: true,
+        autoIndex: false
+    })
+        .then((res) => console.log(`DATABASE connection SUCCESSFULY`))
+        .catch((err) => {
+            console.log(err);
+            process.exit();
+        });
+}
+
+module.exports.connectDB = connectDB;
