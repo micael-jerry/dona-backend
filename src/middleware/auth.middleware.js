a93152afe389458952aa4d631c17fbf241d63fca
Micael Jerry <hei.jerry.2@gmail.com>
2024-04-29 10:16:38 +0300

    chore: auth verification method

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.verifyAuth = (req, res, next) => {
+	const bearerToken = req.headers.authorization;
+	try {
+		const token = bearerToken.split(' ')[1];
+		jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodedToken) => {
+			if (err) {
+				res.status(401).json({
+					message: 'Unauthorized',
+				});
+			} else {
+				req.user = {
+					userId: decodedToken.userId,
+				}
+				next();
+			}
+		})
+	} catch (error) {
+		res.status(401).json({
+			message: 'Unauthorized',
+		})
+	}
+}
