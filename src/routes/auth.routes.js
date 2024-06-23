36e3e15ce983a10a96a94ce32a4823c1ca57585c
Micael Jerry <hei.jerry.2@gmail.com>
2024-06-23 10:18:07 +0300

    fix: update print width prettier

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { signUp, login } = require('../controller/auth.controller');
-const {
-	signUpBodyValidator,
-	loginBodyValidator,
-} = require('../middleware/validator/auth.validator');
+const { signUpBodyValidator, loginBodyValidator } = require('../middleware/validator/auth.validator');
 
 router.post('/register', signUpBodyValidator, signUp);
 router.post('/login', loginBodyValidator, login);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,13 @@
 const express = require('express');
-const {
-	getAllUsers,
-	getUserInfo,
-	updateUser,
-} = require('../controller/user.controller');
-const {
-	updateUserValidator,
-} = require('../middleware/validator/user.validator');
-const {
-	authentication,
-} = require('../middleware/auth/authentication.middleware');
+const { getAllUsers, getUserInfo, updateUser } = require('../controller/user.controller');
+const { updateUserValidator } = require('../middleware/validator/user.validator');
+const { authentication } = require('../middleware/auth/authentication.middleware');
 const { himself } = require('../middleware/auth/authorization.middleware');
 const router = express.Router();
 
 // User operation
 router.get('/', getAllUsers);
 router.get('/:user_id', authentication, himself, getUserInfo);
-router.put(
-	'/:user_id',
-	authentication,
-	himself,
-	updateUserValidator,
-	updateUser,
-);
+router.put('/:user_id', authentication, himself, updateUserValidator, updateUser);
 
 module.exports.userRouter = router;
