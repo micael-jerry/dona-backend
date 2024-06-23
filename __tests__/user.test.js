5adf404b06403ddd3cce2e1d332858a4b664b2b1
Micael Jerry <hei.jerry.2@gmail.com>
2024-06-23 17:50:30 +0300

    fix: test update user

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,7 +2,13 @@ const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals');
 const request = require('supertest');
 const app = require('../app');
 const { AUTH_LOGIN_PATH, USERS_PATH } = require('./conf/path');
-const { USER_ONE_EMAIL, USER_ONE_PASSWORD, USER_TWO_ID, USER_TWO_EMAIL } = require('./conf/test.utils');
+const {
+	USER_ONE_EMAIL,
+	USER_ONE_PASSWORD,
+	USER_TWO_ID,
+	USER_TWO_EMAIL,
+	USER_TWO_PASSWORD,
+} = require('./conf/test.utils');
 const { connectDB, disconnectDB } = require('../src/config/db');
 const { StatusCodes } = require('http-status-codes');
 
@@ -43,6 +49,21 @@ describe(`Test /users`, () => {
 		expect(res.body.email).toBe(USER_TWO_EMAIL);
 	});
 
+	it('should updated user', async () => {
+		const logRes = await request(app)
+			.post(AUTH_LOGIN_PATH)
+			.send({ email: USER_TWO_EMAIL, password: USER_TWO_PASSWORD })
+			.expect(StatusCodes.OK);
+
+		const res = await request(app)
+			.put(`${USERS_PATH}/${USER_TWO_ID}`)
+			.set({ Authorization: `Bearer ${logRes.body.token}` })
+			.send({ bio: 'new bio' })
+			.expect('Content-Type', /json/)
+			.expect(StatusCodes.OK);
+		expect(res.statusCode).toBe(StatusCodes.OK);
+		expect(res.body.bio).toEqual('new bio');
+	});
 	afterAll(async () => {
 		await disconnectDB();
 	});
